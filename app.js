4b7575a643f560a70acca5abe6d713686b670ea9
Aaron Ma <aaronma113@gmail.com>
2022-09-24 14:12:45 -0400

    added mobile responsiveness

--- app.js
+++ app.js
@@ -4,6 +4,20 @@
 
 let isModalOpen = false;
 let contrastToggle = false;
+const scaleFactor = 1/20
+
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+    console.log(x,y)
+
+    for (let i=0; i< shapes.length; ++i) {
+        const isOdd = i % 2 === 0;
+        const boolInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+    }
+}
 
 function toggleContrast() {
     contrastToggle = !contrastToggle;
@@ -45,3 +59,5 @@ function toggleModal() {
     isModalOpen = true;
     document.body.classList += " modal--open";
 }
+
+
